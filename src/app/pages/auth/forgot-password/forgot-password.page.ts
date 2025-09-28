83c99d2b78d1425d64558195056afcb8bd8a71a1
darkxdave <xdavidrojas@hotmail.com>
2025-09-28 18:24:15 -0300

    nueva ruta principal

---
+++ src/app/pages/auth/forgot-password/forgot-password.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.page.html',
+  styleUrls: ['./forgot-password.page.scss'],
+  standalone: true,
+  imports: [IonicModule]
+})
+export class ForgotPasswordPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.page.html',
+  styleUrls: ['./sign-up.page.scss'],
+  standalone: true,
+  imports: [IonicModule]
+})
+export class SignUpPage {}
